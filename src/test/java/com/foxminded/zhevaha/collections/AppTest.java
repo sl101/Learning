63a48009c88e28d71bbf893929e3c9014fb04634
Viacheslav Zhevaha <v.zhevaga@gmail.com>
2016-12-30 00:30:27 +0200

    test refactor

--- src/test/java/com/foxminded/zhevaha/collections/AppTest.java
+++ src/test/java/com/foxminded/zhevaha/collections/AppTest.java
@@ -16,27 +16,16 @@ public class AppTest {
 
 	@Test
 	public void testSpeedCaching() {
-		String example = "Write an application that takes a string and returns the number of unique characters in the string. It is expected that a string with the same character sequence may be passed several times to the method. Since the counting operation can be time consuming, the method should cache the results, so that when the method is given a string previously encountered, it will simply retrieve the stored result. Use collections and maps where appropriate.";
-		String secondExample = example;
-		String[] examples = { example, secondExample };
-		long cacheWritingTime = 0;
-		long cacheReadTime = 0;
-		for (int i = 0; i < examples.length; i++) {
-			if (i == 0) {
-				cacheWritingTime = count(examples, i);
-			} else {
-				cacheReadTime = count(examples, i);
-			}
-		}
-		assertTrue(cacheReadTime < cacheWritingTime);
-	}
-
-	private long count(String[] equalExamples, int i) {
-		long result;
-		result = System.currentTimeMillis();
-		uniqueSymbols.count(equalExamples[i]);
-		result = System.currentTimeMillis() - result;
-		return result;
+		String example = "Write an application that takes a string and returns the number of unique characters in the string. It is expected that a string with the same character sequence may be passed several times to the method. Since the counting operation can be time consuming, the method should cache the results, so that when the method is given a string previously encountered, it will simply retrieve the stored result. Use collections and maps where appropriate.";		
+		long writeCacheTime = System.currentTimeMillis();
+		uniqueSymbols.count(example);
+		writeCacheTime = System.currentTimeMillis() - writeCacheTime;
+		
+		long readCacheTime = System.currentTimeMillis();
+		uniqueSymbols.count(example);
+		readCacheTime = System.currentTimeMillis() - readCacheTime;
+		
+		assertTrue(readCacheTime < writeCacheTime);
 	}
 
 	@Test
