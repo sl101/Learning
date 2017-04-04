8b7186208989c3da6e1cd6252688748684d5d4b2
Andrey Bobrow <aquariusmaster@yandex.ru>
2017-04-04 21:08:34 +0300

    feature #14 fix test failed

--- src/test/java/ua/com/foxminded/serviceacc/repository/ContactTypeRepositoryTest.java
+++ src/test/java/ua/com/foxminded/serviceacc/repository/ContactTypeRepositoryTest.java
@@ -1,5 +1,6 @@
 package ua.com.foxminded.serviceacc.repository;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -11,4 +12,9 @@ import ua.com.foxminded.serviceacc.config.PersistenceConfig;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {PersistenceConfig.class})
 public class ContactTypeRepositoryTest {
+
+    @Test
+    public void saveContactType(){
+
+    }
 }
