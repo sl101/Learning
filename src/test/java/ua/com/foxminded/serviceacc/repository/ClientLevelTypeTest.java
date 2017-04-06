7f3aa66ebf254fcb739fe381e7d5a8aead15448b
Andrey Bobrow <aquariusmaster@yandex.ru>
2017-04-06 11:21:38 +0300

    add test for ClientLevelType

---
+++ src/test/java/ua/com/foxminded/serviceacc/repository/ClientLevelTypeTest.java
@@ -0,0 +1,70 @@
+package ua.com.foxminded.serviceacc.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.postgresql.util.PSQLException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ua.com.foxminded.serviceacc.config.PersistenceConfig;
+import ua.com.foxminded.serviceacc.model.ClientLevelType;
+import ua.com.foxminded.serviceacc.model.ClientStatusType;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static ua.com.foxminded.serviceacc.repository.ModelBuilder.*;
+
+/**
+ * Created by andreb on 06.04.17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {PersistenceConfig.class})
+public class ClientLevelTypeTest {
+    @Autowired
+    ClientRepository clientRepository;
+    @Autowired
+    ClientLevelTypeRepository clientLevelTypeRepository;
+    @Autowired
+    PersonRepository personRepository;
+
+    @Before
+    public void fillClientStatusType(){
+        List<ClientLevelType> statuses = ModelBuilder.buildListTestClientLevelType();
+        clientLevelTypeRepository.save(statuses);
+        assertThat(clientLevelTypeRepository.findAll(), hasSize(3));
+    }
+
+    @After
+    public void deleteData(){
+        clientRepository.deleteAll();
+        clientLevelTypeRepository.deleteAll();
+        personRepository.deleteAll();
+    }
+
+    @Test(expected = DataIntegrityViolationException.class)
+    public void saveTheSameStatusTest() {
+        ClientLevelType doubleBeginner = new ClientLevelType("Beginner");
+        clientLevelTypeRepository.save(doubleBeginner); //Must throws error duplicate key value violates unique constraint
+
+        assertThat(clientLevelTypeRepository.findAll(), hasSize(3));
+    }
+
+    @Test
+    public void getLevelByStringTest(){
+        ClientLevelType level = clientLevelTypeRepository.findOneByLevel(BEGINNER);
+        assertThat(level, notNullValue());
+        assertThat(level.getLevel(), is(BEGINNER));
+        level = clientLevelTypeRepository.findOneByLevel(REGULAR);
+        assertThat(level, notNullValue());
+        assertThat(level.getLevel(), not(BEGINNER));
+        assertThat(level.getLevel(), is(REGULAR));
+    }
+}
