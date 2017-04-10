7ef7dce801635c92e111f60897f6fd2228600bad
Lifyrenko Anton <alifyrenko@gmail.com>
2017-04-10 12:53:43 +0300

    removed lombok and genereted getter-setter (#29)

--- src/main/java/ua/com/foxminded/serviceacc/controller/ClientStatusTypeController.java
+++ src/main/java/ua/com/foxminded/serviceacc/controller/ClientStatusTypeController.java
@@ -1,10 +1,7 @@
 package ua.com.foxminded.serviceacc.controller;
 
-import lombok.Getter;
-import lombok.Setter;
-import ua.com.foxminded.serviceacc.controller.client.ClientController;
-import ua.com.foxminded.serviceacc.model.ClientStatusType;
-import ua.com.foxminded.serviceacc.service.ClientStatusTypeService;
+import java.io.Serializable;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -12,78 +9,106 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.Serializable;
-import java.util.List;
+
+import ua.com.foxminded.serviceacc.controller.client.ClientController;
+import ua.com.foxminded.serviceacc.model.ClientStatusType;
+import ua.com.foxminded.serviceacc.service.ClientStatusTypeService;
 
 @Named
 @SessionScoped
 @ManagedBean(name = "ClientStatusTypeController")
-@Getter @Setter
 public class ClientStatusTypeController implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private String code;
-    private String title;
-    private String id;
+	private static final long serialVersionUID = 1L;
+	private String code;
+	private String title;
+	private String id;
 
-    private static boolean isShowForm = false;
+	private static boolean isShowForm = false;
 
-    public boolean getIsShowForm() {
-        return isShowForm;
-    }
+	public boolean getIsShowForm() {
+		return isShowForm;
+	}
 
-    public void setIsShowForm(boolean isShowForm) {
-        this.isShowForm = isShowForm;
-    }
+	public void setIsShowForm(boolean isShowForm) {
+		this.isShowForm = isShowForm;
+	}
 
-    @Inject
-    @ManagedProperty(value = "#{clientStatusTypeService}")
-    ClientStatusTypeService clientStatusTypeService;
+	@Inject
+	@ManagedProperty(value = "#{clientStatusTypeService}")
+	ClientStatusTypeService clientStatusTypeService;
 
-    @Inject
-    @ManagedProperty(value = "#{clientController}")
-    ClientController clientController;
+	@Inject
+	@ManagedProperty(value = "#{clientController}")
+	ClientController clientController;
 
-    private List<ClientStatusType> statusList;
+	private List<ClientStatusType> statusList;
 
-    @PostConstruct
-    public void init() {
-        clientStatusTypeService.findAll();
-    }
+	@PostConstruct
+	public void init() {
+		clientStatusTypeService.findAll();
+	}
 
-    public List<ClientStatusType> getStatusList () {
-        return clientStatusTypeService.findAll();
-    }
+	public List<ClientStatusType> getStatusList() {
+		return clientStatusTypeService.findAll();
+	}
 
-    public void save() {
-        if("".equals(id)) {
-            ClientStatusType newStatus = new ClientStatusType(code, title);
-            clientStatusTypeService.save(newStatus);
-        } else {
-            ClientStatusType status = clientStatusTypeService.findById(Long.parseLong(id));
-            status.setCode(code);
-            status.setTitle(title);
-            clientStatusTypeService.update(status);
-        }
-        id=""; code=""; title="";
-    }
+	public void save() {
+		if ("".equals(id)) {
+			ClientStatusType newStatus = new ClientStatusType(code, title);
+			clientStatusTypeService.save(newStatus);
+		} else {
+			ClientStatusType status = clientStatusTypeService.findById(Long.parseLong(id));
+			status.setCode(code);
+			status.setTitle(title);
+			clientStatusTypeService.update(status);
+		}
+		id = "";
+		code = "";
+		title = "";
+	}
 
-    public void edit(ClientStatusType status) {
-        code = status.getCode();
-        title = status.getTitle();
-        id = status.getId().toString();
-    }
+	public void edit(ClientStatusType status) {
+		code = status.getCode();
+		title = status.getTitle();
+		id = status.getId().toString();
+	}
 
-    public void delete(ClientStatusType status) {
-        clientStatusTypeService.delete(status.getId());
-    }
+	public void delete(ClientStatusType status) {
+		clientStatusTypeService.delete(status.getId());
+	}
 
-    public void showForm() {
-        setIsShowForm(true);
-        clientController.hideAllClient();
-        clientStatusTypeService.findAll();
-    }
+	public void showForm() {
+		setIsShowForm(true);
+		clientController.hideAllClient();
+		clientStatusTypeService.findAll();
+	}
 
-    public void hideForm() {
-        setIsShowForm(false);
-    }
+	public void hideForm() {
+		setIsShowForm(false);
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
 }
--- src/main/java/ua/com/foxminded/serviceacc/model/ClientStatusType.java
+++ src/main/java/ua/com/foxminded/serviceacc/model/ClientStatusType.java
@@ -1,42 +1,78 @@
 package ua.com.foxminded.serviceacc.model;
 
-import lombok.*;
-
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 /**
- * Created by andreb on 04.04.17.
- * Represent Client status
+ * Created by andreb on 04.04.17. Represent Client status
  */
 @Entity
 @Table(name = "client_status_type")
-@Getter @Setter
-@NoArgsConstructor
 public class ClientStatusType {
 
-    @Id
-    @SequenceGenerator(name = "generator", sequenceName = "client_status_type_id_seq")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generator")
-    @Column(name = "id", unique = true, nullable = false)
-    private Long id;
+	@Id
+	@SequenceGenerator(name = "generator", sequenceName = "client_status_type_id_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generator")
+	@Column(name = "id", unique = true, nullable = false)
+	private Long id;
 
-    @Column (name = "code", unique = true, nullable = false)
-    private String code;
+	@Column(name = "code", unique = true, nullable = false)
+	private String code;
 
-    @Column (name = "title", nullable = false)
-    private String title;
+	@Column(name = "title", nullable = false)
+	private String title;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "status", orphanRemoval = true)
-    private Set<Client> clients = new HashSet<>();
-    
-    @Column(name = "active", nullable = false)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "status", orphanRemoval = true)
+	private Set<Client> clients = new HashSet<>();
+
+	@Column(name = "active", nullable = false)
 	private boolean active = true;
 
-    public ClientStatusType(String code, String title) {
-        this.code = code;
-        this.title = title;
-    }	
+	public ClientStatusType(String code, String title) {
+		this.code = code;
+		this.title = title;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Set<Client> getClients() {
+		return clients;
+	}
+
+	public void setClients(Set<Client> clients) {
+		this.clients = clients;
+	}
 
 }
